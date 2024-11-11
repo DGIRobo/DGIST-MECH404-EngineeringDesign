3765d245feadd3fa7f68cd18c8237678c373469a
DGIRobo <48275742+DGIRobo@users.noreply.github.com>
2024-11-11 14:05:18 +0900

    single motor pid controller main code revised

--- Software/3. Motor PID Control/motor_pid_control/main.py
+++ Software/3. Motor PID Control/motor_pid_control/main.py
@@ -14,21 +14,28 @@ if __name__ == '__main__':
 	while True:
 		current_time = datetime.datetime.now().timestamp() # Dimension: sec
 
-		car.FL_motor.motor_state_estimation()
-		car.FR_motor.motor_state_estimation()
-		car.BL_motor.motor_state_estimation()
-		car.BR_motor.motor_state_estimation()
+		car.car_state_estimation()
 
 		print('-----------------------------------------------------------')
 		print(f'current time: {current_time - starting_time}')
 		print(f'time interval: {current_time - previous_time}')
+		print('-----------------------------------------------------------')
+		print(f'position of FL wheel: {car.FL_motor._incremental_pos}')
+		print(f'position of FR wheel: {car.FR_motor._incremental_pos}')
+		print(f'position of BL wheel: {car.BL_motor._incremental_pos}')
+		print(f'position of BR wheel: {car.BR_motor._incremental_pos}')
 		print('-----------------------------------------------------------')
 		print(f'velocity of FL wheel: {car.FL_motor._vel}')
 		print(f'velocity of FR wheel: {car.FR_motor._vel}')
 		print(f'velocity of BL wheel: {car.BL_motor._vel}')
 		print(f'velocity of BR wheel: {car.BR_motor._vel}')
 
-		car.Joystick_PWM_Controller()
+		# target motor setting
+		target_motor = 'FL_motor'
+		# position PID
+		#car.single_motor_incremental_pos_control(target_motor, 2*3.14, 1, 0, 0)
+		# velocity PID
+		#car.single_motor_vel_control(target_motor, 1, 1, 0, 0)
 		
 		previous_time = datetime.datetime.now().timestamp() # Dimension: sec
 
